4b62d817e85d562bf5ae4cf240f141299d555718
Catherine Lan <yl4289@columbia.edu>
2021-04-14 03:34:28 +0800

    edited init file module loading syntax

--- LOCOA/__init__.py
+++ LOCOA/__init__.py
@@ -6,6 +6,10 @@ Initialize program
 
 __version__ = "0.0.2"
 
-import src.dataloader
-import src.analysis
-import src.plot
+#import src.dataloader
+#import src.analysis
+#import src.plot
+
+from LOCOA.dataloader import Dataloader
+from LOCOA.analysis import Analysis
+from LOCOA.plot import Plot
